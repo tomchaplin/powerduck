6b3bcc2331f7d08ab7e9e9d3f18f439ee5b5533f
Thomas Chaplin <tomrchaplin@gmail.com>
2020-08-06 09:05:34 +0100

    Add sudo to install script

--- install.sh
+++ install.sh
@@ -4,21 +4,24 @@ INSTALL_DIR="/opt/powerduck"
 RELEASE="https://raw.githubusercontent.com/tomchaplin/powerduck/master/dist/powerduck.zip"
 BIN_DIR="/usr/local/bin"
 
+# Remove old symlinks
+sudo unlink "$BIN_DIR/powerduck"
+sudo unlink "$BIN_DIR/powerduckling"
 # Clean install directory
 if [[ -d "$INSTALL_DIR" ]]; then
 	rm -r "$INSTALL_DIR"
 fi
 # Make install directory
-mkdir "$INSTALL_DIR"
+sudo mkdir "$INSTALL_DIR"
 # Get release
 cd "$INSTALL_DIR"
-curl "$RELEASE" --output powerduck.zip
-unzip powerduck.zip
-rm powerduck.zip
-mv powerduck-release/* .
-rm -r powerduck-release
+sudo curl "$RELEASE" --output powerduck.zip
+sudo unzip powerduck.zip
+sudo rm powerduck.zip
+sudo mv powerduck-release/* .
+sudo rm -r powerduck-release
 # Initializse config files
 bash "$INSTALL_DIR/init.sh"
 # Install binary symlinks
-ln -s "$INSTALL_DIR/powerduck-linux" "$BIN_DIR/powerduck"
-ln -s "$INSTALL_DIR/powerduckling" "$BIN_DIR/powerduckling"
+sudo ln -s "$INSTALL_DIR/powerduck-linux" "$BIN_DIR/powerduck"
+sudo ln -s "$INSTALL_DIR/powerduckling" "$BIN_DIR/powerduckling"
